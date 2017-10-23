4455bc796eb22816e90599a647ea3f09f8672c0d
Mikko Pohja <mikko.pohja@futurice.com>
2017-10-23 07:16:45 +0300

    Fixed RF24 initializing

--- rf24.js
+++ rf24.js
@@ -4,6 +4,10 @@ function connectUSB(devicePath) {
     const port = new SerialPort(devicePath, { 
         baudRate: 9600
     });
+    port.on("open", () => {
+        console.log("Serial port open");
+    });
+
     return port.pipe(new SerialPort.parsers.Readline({ delimiter: '}' }));
 }
 
@@ -14,24 +18,17 @@ module.exports = (usbDevice, sendValue) => {
 
     const port = connectUSB(usbDevice);
 
-    port.on("open", () => {
-        console.log("Serial port open");
-        let message = "";
-        let onGoingMessage = false;
-        port.on("data", data => {
-            let newData = data.toString("ascii") + "}";
-            console.log("New data | ", newData, "|");
-            try{
-                let parsedMessage = JSON.parse(newData);
-                console.log("Sensordata", JSON.stringify(parsedMessage, null, 2));
-                
-                sendValue(parsedMessage.address, parsedMessage.value);
-            }
-            catch(e) {
-                console.log("Something wrong parsing message");
-                console.log(e);
-            }
-        });
+    port.on("data", data => {
+        let newData = data.toString("ascii") + "}";
+        console.log("New data | ", newData, "|");
+        try{
+            let parsedMessage = JSON.parse(newData);
+            console.log("Sensordata", JSON.stringify(parsedMessage, null, 2));
+            sendValue(parsedMessage.address, parsedMessage.value);
+        }
+        catch(e) {
+            console.log("Something wrong parsing message");
+            console.log(e);
+        }
     });
-
 }
